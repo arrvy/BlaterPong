7462cc180aea6f6f7657519615fb875d982c2bb4
arrvy <nashr.wahyono@mhs.unsoed.ac.id>
2024-12-21 19:46:30 +0700

    Update button.hpp

--- button.hpp
+++ button.hpp
@@ -1,14 +1,15 @@
 #pragma once
 #include <raylib.h>
 
-class Button
-{
-    public:
-        Button(const char* imagePath, Vector2 imagePosition, float scale);
-        ~Button();
-        void Draw();
-        bool isPressed(Vector2 mousePos, bool mousePressed);
-    private:
-        Texture2D texture;
-        Vector2 position;
+class Button {
+public:
+    Button(const char *imagePath, Vector2 imagePosition, float scale);
+    ~Button();
+
+    void Draw(Vector2 mousePos); // Tambahkan parameter mousePos
+    bool isPressed(Vector2 mousePos, bool mousePressed);
+
+private:
+    Texture2D texture;
+    Vector2 position;
 };
